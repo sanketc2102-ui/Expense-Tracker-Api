bef9f9177b3e811818cc336b6ed72ddf8075e304
Sanket Chavan <sanketc2102@gmail.com>
2026-06-06 13:29:18 +0530

    feat(server): configures cors, json parsing, and static file serving

--- src/app.js
+++ src/app.js
@@ -1,5 +1,20 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ limit: "16kb", extended: true }));
+app.use(express.static("public"));
+
+// cors configuration
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 
 export default app;
