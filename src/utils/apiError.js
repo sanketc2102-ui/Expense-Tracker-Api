a1d84409f2a15698943134aff08b05bdb6f80d3e
Sanket Chavan <sanketc2102@gmail.com>
2026-06-06 13:50:33 +0530

    feat(utils) add ApiResponse and ApiError standard classes

---
+++ src/utils/apiError.js
@@ -0,0 +1,17 @@
+class ApiError extends Error {
+  constructor(statusCode, message, errors = [], stack) {
+    super(message);
+    this.statuscode = statusCode;
+    this.message = message;
+    this.errors = errors;
+    this.success = false;
+
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.stackTraceLimit(this, this.constructor);
+    }
+  }
+}
+
+export { ApiError };
